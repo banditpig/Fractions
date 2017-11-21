1754d2d27762cabac89d52014aa92f5bb962540d
banditpig <mike_k_houghton@yahoo.co.uk>
2017-11-21 22:41:37 +0000

    in progress

--- FractionParser.hs
+++ FractionParser.hs
@@ -1,30 +1,31 @@
 module FractionParser where
+import           Control.Monad
 import           Data.List
 import           Fractions
 import           Text.Megaparsec
 import           Text.Megaparsec.String
-
 type Digit  = Integer
 type First  = Digit
-type Second = [Digit]
 type Repeat = [Digit]
 
+-- [a1;a2,a3,a4...] => first = a1, repeat = [a2,a3,a4]
 data FracStruc = FracStruc { first :: First, repeat :: Repeat} deriving (Show)
+
 -- right and left bird :). Put the parser in the middle
 dropSpaces :: Parser a -> Parser a
 dropSpaces p = space *> p <* space
 
--- A number followed by a separator char - typically ; or ,
+-- A number followed by a separator char - for Fractions this will be ; or ,
 digitSep :: Char -> Parser Digit
-digitSep = dropSpaces . digitSep' where
-        digitSep'  :: Char -> Parser Digit
-        digitSep'  ch = do
+digitSep = dropSpaces . digitSep1 where
+        digitSep1 :: Char -> Parser Digit
+        digitSep1  ch = do
             d <- some digitChar
             space
             many . char $ ch
             return (read d)
 
--- Parse out the a1 in [a1;a2, a3...]
+-- Parse out the a1 in [a1;a2,a3...]
 firstP :: Parser Digit
 firstP = dropSpaces . digitSep $ ';'
 
@@ -51,7 +52,15 @@ genFa (FracStruc fs rep) =
     \n -> if n == 0 then Numbr fs else Numbr (g n) where
         g n = rep !! ix where
             ix = rem (fromIntegral (n - 1)) (length rep)
+-- From the supplied list format and the given depth perhaps create a Fraction
+fracFromList :: Integer ->  String -> Maybe Fraction
+fracFromList depth s =
+    case makeStruct s of
+        Nothing    -> Nothing
+        Just struc -> Just $ frac depth struc
 
+-- Create a fraction to the supplied depth using the given FracStruc
+-- fb assumed fixed at 1
 frac :: Integer -> FracStruc -> Fraction
 frac depth struc@(FracStruc fs rep)
     | null rep = Numbr fs
@@ -60,6 +69,9 @@ frac depth struc@(FracStruc fs rep)
         fb = const 1
 
 fa12 = genFa (FracStruc 1 [2])
+
+evalFracM ::  Fraction -> Maybe Float
+evalFracM  = Just . evalFrac
 
 root :: Integer -> Integer -> Fraction
 root n = contFrac fa fb where
